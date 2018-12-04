a329379ece83f36ea577b5b2e743345db67eaa42
ArttuLaine <45590658+ArttuLaine@users.noreply.github.com>
2018-12-04 14:05:38 +0200

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,9 +4,12 @@ class HelloWorld
     public static void main(String args[]) 
     { 
 		String s = "";
+		String sb = "";
 		if (args.length<1) s="Hello World";
 		else s=args[0];
+		sb=new StringBuilder(s).reverse().toString() ;
 
 	System.out.println(s); 
+	System.out.println(sb); 
     } 
 }
